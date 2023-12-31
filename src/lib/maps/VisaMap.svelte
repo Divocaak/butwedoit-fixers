6ecebf4372f090382bc30cb0c87c8dd80298fc66
Vojtěch divoký <divokyvojta@gmail.com>
2023-12-31 17:31:57 +0100

    update 281123 #2

--- src/lib/maps/VisaMap.svelte
+++ src/lib/maps/VisaMap.svelte
@@ -12,23 +12,85 @@
 	import SouthAmerica from '$lib/maps/vancatasvg/SouthAmerica.svelte';
 	import UnitedKingdom from '$lib/maps/vancatasvg/UnitedKingdom.svelte';
 	import UnitedStates from '$lib/maps/vancatasvg/UnitedStates.svelte';
+	import VancataSvgWrapper from '$lib/maps/vancatasvg/VancataSvgWrapper.svelte';
+
+	/* TODO cleanup to separate js file (redundant as its the same as HomeMap) */
+	import { onMount } from 'svelte';
+
+	let mapParent;
+	let mapContainer;
+
+	function transformMap() {
+		// ::before = center
+		const mapParentBefore = window.getComputedStyle(mapParent, ':before');
+		// ::after = end of visible part
+		const mapParentAfter = window.getComputedStyle(mapParent, ':after');
+
+		const mapContainerBefore = window.getComputedStyle(mapContainer, ':before');
+		const mapContainerAfter = window.getComputedStyle(mapContainer, ':after');
+
+		const mapParentBeforeLeft = parseFloat(mapParentBefore.left);
+		const mapContainerBeforeLeft = parseFloat(mapContainerBefore.left);
+		const mapParentAfterLeft = parseFloat(mapParentAfter.left);
+		const mapContainerAfterLeft = parseFloat(mapContainerAfter.left);
+
+		const mapParentBeforeTop = parseFloat(mapParentBefore.top);
+		const mapContainerBeforeTop = parseFloat(mapContainerBefore.top);
+
+		const mapParentWidth = mapParentAfterLeft - mapParentBeforeLeft;
+		const mapContainerWidth = mapContainerAfterLeft - mapContainerBeforeLeft;
+		const scale = mapParentWidth / mapContainerWidth;
+
+		const xOffset = mapParentBeforeLeft - mapContainerBeforeLeft;
+		const yOffset = mapParentBeforeTop - mapContainerBeforeTop;
+
+		mapContainer.style.transform = `scale(${scale}) translate(${xOffset}px, ${yOffset}px)`;
+	}
+
+	onMount(() => {
+		transformMap();
+		window.addEventListener('resize', debounce(transformMap, 200, false), false);
+		return () => {
+			window.removeEventListener('resize', transformMap);
+		};
+	});
+
+	function debounce(func, delay) {
+		let timeoutId;
+
+		return function () {
+			const context = this;
+			const args = arguments;
+
+			clearTimeout(timeoutId);
+
+			timeoutId = setTimeout(function () {
+				func.apply(context, args);
+			}, delay);
+		};
+	}
 </script>
 
-<div class="map-parent">
-	<div class="map-container">
-		<Africa classes="country" />
-		<Asia classes="country" />
-		<Australia classes="country" />
-		<Canada classes="country" />
-		<Europe classes="country" />
-		<India classes="country" />
-		<Japan classes="country" />
-		<Mexico classes="country" />
-		<MiddleEast classes="country" />
-		<Russia classes="country" />
-		<SouthAmerica classes="country" />
-		<UnitedKingdom classes="country" />
-		<UnitedStates classes="country" />
+<div class="map-parent" bind:this={mapParent}>
+	<div class="map-container" bind:this={mapContainer}>
+		<VancataSvgWrapper label="Russia" labelTop={0} labelLeft={0}><Russia /></VancataSvgWrapper>
+		<VancataSvgWrapper label="Africa" labelTop={0} labelLeft={0}><Africa /></VancataSvgWrapper>
+		<VancataSvgWrapper label="Asia" labelTop={0} labelLeft={0}><Asia /></VancataSvgWrapper>
+		<VancataSvgWrapper label="Australia" labelTop={0} labelLeft={0}><Australia /></VancataSvgWrapper
+		>
+		<VancataSvgWrapper label="Canada" labelTop={0} labelLeft={0}><Canada /></VancataSvgWrapper>
+		<VancataSvgWrapper label="Europe" labelTop={0} labelLeft={0}><Europe /></VancataSvgWrapper>
+		<VancataSvgWrapper label="India" labelTop={0} labelLeft={0}><India /></VancataSvgWrapper>
+		<VancataSvgWrapper label="Japan" labelTop={0} labelLeft={0}><Japan /></VancataSvgWrapper>
+		<VancataSvgWrapper label="Mexico" labelTop={0} labelLeft={0}><Mexico /></VancataSvgWrapper>
+		<VancataSvgWrapper label="Middle East" labelTop={0} labelLeft={0}
+			><MiddleEast /></VancataSvgWrapper
+		>
+		<VancataSvgWrapper label="South America" labelTop={0} labelLeft={0}
+			><SouthAmerica /></VancataSvgWrapper
+		>
+		<VancataSvgWrapper label="UK" labelTop={0} labelLeft={0}><UnitedKingdom /></VancataSvgWrapper>
+		<VancataSvgWrapper label="USA" labelTop={0} labelLeft={0}><UnitedStates /></VancataSvgWrapper>
 	</div>
 </div>
 
@@ -37,32 +99,44 @@
 		position: relative;
 		width: 100vw;
 		height: 75vh;
-		background-color: blue;
+	}
+
+	/* position anchor point */
+	.map-parent::before {
+		content: '';
+		position: absolute;
+		top: 50%;
+		left: 50vw;
+	}
+
+	/* scale point */
+	.map-parent::after {
+		content: '';
+		position: absolute;
+		top: 50%;
+		right: 5vw;
 	}
 
 	.map-container {
 		position: absolute;
-		transform: scale(10);
-		/* BUG responsive */
-		top: -200px;
-		left: 100px;
-	}
-
-	:global(.country) {
-		position: absolute;
+		width: 100%;
+		height: 100%;
 		transform: scale(1);
-		display: block;
-		padding: 0;
-		margin: 0;
-
-		fill: var(--grey);
-		stroke: #767676;
-		stroke-width: 0.1rem;
-		stroke-linejoin: arcs;
-		z-index: 1;
-		pointer-events: none;
 	}
 
-    /* URGENT wrap every country in div with .country class to be able to hover fx (hope so)
-    same as homemap */
+	/* position anchor point */
+	.map-container::before {
+		content: '';
+		position: absolute;
+		top: 50px;
+		left: 50px;
+	}
+
+	/* scale point */
+	.map-container::after {
+		content: '';
+		position: absolute;
+		top: 50px;
+		left: 110px;
+	}
 </style>
---
+++ src/lib/maps/vancatasvg/VancataSvgWrapper.svelte
@@ -0,0 +1,58 @@
+<script>
+	export let top = 0;
+	export let left = 0;
+    export let label;
+	export let labelTop = 0;
+	export let labelLeft = 0;
+</script>
+
+<div>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="country"
+		style="top:{top}px; left:{left}px;"
+		width="100"
+		height="100"
+		viewBox="0 0 6000 4000"
+	>
+		<slot />
+	</svg>
+	<p class="label unbounded" style="top:{labelTop}px; left:{labelLeft}px;">{label}</p>
+</div>
+
+<style>
+	.country {
+		position: absolute;
+		transform: scale(1);
+		display: block;
+		padding: 0;
+		margin: 0;
+
+		fill: var(--grey);
+		stroke: #767676;
+		stroke-width: 0.1rem;
+		stroke-linejoin: arcs;
+		z-index: 7;
+		pointer-events: none;
+
+		transition: all 0.2s ease-in-out;
+	}
+
+	:global(.country > path, .country > polygon) {
+		pointer-events: all;
+	}
+
+	.country:hover {
+		fill: var(--yellow);
+        stroke: var(--yellow);
+        stroke-width: .2rem;
+	}
+
+    .label{
+        position: absolute;
+        top: 10px;
+        left: 0px;
+        font-size: .1rem;
+        z-index: 8;
+    }
+</style>
